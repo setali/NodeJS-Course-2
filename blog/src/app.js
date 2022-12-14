9af57375407237c0d37bf667373071e4b6e990f5
SAli <set.ali.mousavi@gmail.com>
2022-12-14 18:51:38 +0330

    6-custom-engine

--- blog/src/app.js
+++ blog/src/app.js
@@ -1,15 +1,29 @@
+import path from 'path'
+import fs from 'fs'
 import express from 'express'
 import router from './routes/index.js'
 import errorHandler from './middlewares/error-handler'
-import renderTemplate from './middlewares/render-template'
-
-global.__basedir = __dirname
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.use(renderTemplate)
+app.engine('ali', (filePath, params, callback) => {
+  let view = fs.readFileSync(filePath, 'utf-8')
+
+  const entries = Object.entries(params)
+
+  entries.forEach(([key, value]) => {
+    if (typeof value === 'string') {
+      view = view.replace(`#${key}#`, value)
+    }
+  })
+
+  return callback(null, view)
+})
+
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'ali')
 
 const port = 8080
 
--- blog/src/controllers/general.js
+++ blog/src/controllers/general.js
@@ -1,19 +1,19 @@
 export function home (req, res) {
-  res.renderTemplate('index', {
+  res.render('index', {
     title: 'Home Page',
     content: 'This is homepage'
   })
 }
 
 export function about (req, res) {
-  res.renderTemplate('index', {
+  res.render('index', {
     title: 'About us',
     content: 'This is About us page'
   })
 }
 
 export function contact (req, res) {
-  res.renderTemplate('index', {
+  res.render('index', {
     title: 'Contact us',
     content: 'This is About us page'
   })
