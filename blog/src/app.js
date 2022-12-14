a4425158409223967a688f1f556ca77bd85c1184
SAli <set.ali.mousavi@gmail.com>
2022-12-14 19:16:42 +0330

    7-pug

--- blog/src/app.js
+++ blog/src/app.js
@@ -8,22 +8,8 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.engine('ali', (filePath, params, callback) => {
-  let view = fs.readFileSync(filePath, 'utf-8')
-
-  const entries = Object.entries(params)
-
-  entries.forEach(([key, value]) => {
-    if (typeof value === 'string') {
-      view = view.replace(`#${key}#`, value)
-    }
-  })
-
-  return callback(null, view)
-})
-
 app.set('views', path.resolve(__dirname, 'views'))
-app.set('view engine', 'ali')
+app.set('view engine', 'pug')
 
 const port = 8080
 
